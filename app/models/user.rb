8c2af003277f33fa8da9b67a491baea34dda52e7
takahirohirayama <honobonohinatabokko@gmail.com>
2021-03-13 22:32:59 +0900

    with_optionsに変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname,         presence: true
-  validates :family_name,      presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々]/ }
-  validates :first_name,       presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々]/ }
-  validates :family_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :first_name_kana,  presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :birthday,         presence: true
-  validates :password,                         format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
-
+  with_options presence: true do
+    validates :nickname        
+    validates :family_name,      format: { with: /\A[ぁ-んァ-ン一-龥々]/ }
+    validates :first_name,       format: { with: /\A[ぁ-んァ-ン一-龥々]/ }
+    validates :family_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :first_name_kana,  format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :birthday
+    validates :password,         format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
+  end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname              {Faker::Name.initials(number: 2)}
     email                 {Faker::Internet.free_email}
-    password              {Faker::Internet.password(min_length: 6)}
+    password              {'hoge123'}
     password_confirmation {password}
     family_name           {'性'}
     first_name            {'名'}
