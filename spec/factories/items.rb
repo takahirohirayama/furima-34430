0094dbb7d4bf1149c6330eba8a01eef0df119450
takahirohirayama <honobonohinatabokko@gmail.com>
2021-03-15 23:56:02 +0900

    itemモデル単体テストexample書き出し

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,18 @@
 FactoryBot.define do
   factory :item do
-    
+    name                {"aaaaa"}
+    description         {"aaaa"}
+    category_id         {"2"}
+    status_id           {"2"}
+    shipping_charge_id  {"2"}
+    prefecture_id       {"2"}
+    days_to_ship_id     {"2"}
+    price               {"2000"}
+
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,42 @@
 require 'rails_helper'
+describe Item do
+  before do
+    @item = FactoryBot.build(:item)
+  end
 
-RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "商品出品登録" do
+    context "商品出品登録がうまくいくとき" do  
+      it "すべての項目が入力されれば商品登録ができる" do
+      end
+      it "priceが¥300~¥9,999,999であれば登録できる" do
+      end
+    end
+
+    context "商品出品登録がうまくいかないとき" do
+      it "画像が空だと登録できない" do
+      end
+      it "商品名が空だと登録できない" do
+      end
+      it "商品説明がないと登録できない" do
+      end
+      it "カテゴリーが選択されていないと登録できない" do
+      end
+      it "商品の状態が選択されていないと登録できない" do
+      end
+      it "配送料の負担が選択されていないと登録できない" do
+      end
+      it "配送元の地域が選択されていないと登録できない" do
+      end
+      it "配送までの日数が選択されていないと登録できない" do
+      end
+      it "販売価格が空だと登録できない" do
+      end
+      it "販売価格が¥300未満だと登録できない" do
+      end
+      it "販売価格が¥10,000,000以上だと登録できない" do
+      end
+      it "販売価格の記載が半角数字以外だと登録できない" do
+      end
+    end
+  end
 end
