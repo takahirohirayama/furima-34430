fc3ddbd9231f4e8ef3e7903be12c5a120c09a3c0
takahirohirayama <honobonohinatabokko@gmail.com>
2021-03-16 19:35:49 +0900

    単体テスト異常系example追加

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -2,12 +2,12 @@ FactoryBot.define do
   factory :item do
     name                { "aaaaa" }
     description         { "aaaa" }
-    category_id         { "2" }
-    status_id           { "2" }
-    shipping_charge_id  { "2" }
-    prefecture_id       { "2" }
-    days_to_ship_id     { "2" }
-    price               { "2000" }
+    category_id         { 2 }
+    status_id           { 2 }
+    shipping_charge_id  { 2 }
+    prefecture_id       { 2 }
+    days_to_ship_id     { 2 }
+    price               { 2000 }
 
     association :user
 
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -87,6 +87,31 @@ describe Item do
         @item.valid?
         expect(@item.errors.full_messages).to include('Price Half-width number')
       end
+      it 'カテゴリー選択で1の場合は登録できない' do
+        @item.category_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Category Select')
+      end
+      it '商品の状態選択で1の場合は登録できない' do
+        @item.status_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Status Select')
+      end
+      it '配送料負担選択で1の場合は登録できない' do
+        @item.shipping_charge_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Shipping charge Select')
+      end
+      it '配送元の地域選択で0の場合は登録できない' do
+        @item.prefecture_id = 0
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Prefecture Select')
+      end
+      it '配送までの日数選択で1の場合は登録できない' do
+        @item.days_to_ship_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Days to ship Select')
+      end
     end
   end
 end
