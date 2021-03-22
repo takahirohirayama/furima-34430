fdf1aa0447005661534cc252dab4d045fb238e79
takahirohirayama <honobonohinatabokko@gmail.com>
2021-03-22 14:17:24 +0900

    クレジットカード決算実装

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,5 +1,5 @@
 class PurchasesController < ApplicationController
-  before_action :authenticate_user!, except: :index
+  before_action :authenticate_user!
   before_action :set_item, only: [:index, :create]
 
   def index
@@ -9,6 +9,7 @@ class PurchasesController < ApplicationController
   def create
     @purchase_address = PurchaseAddress.new(address_params)
     if @purchase_address.valid?
+      pay_item
       @purchase_address.save
       redirect_to root_path
     else
@@ -20,11 +21,20 @@ class PurchasesController < ApplicationController
   private
 
   def address_params
-    params.require( :purchase_address).permit(:post_cord, :prefecture_id, :city, :address, :building, :phone_num ).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:purchase_address).permit(:post_cord, :prefecture_id, :city, :address, :building, :phone_num ).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
 
   def set_item
     @item = Item.find(params[:item_id])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: address_params[:token],
+      currency: 'jpy'
+    )
   end
 
 end
--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -1,18 +1,20 @@
 class PurchaseAddress
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :post_cord, :prefecture_id, :city, :address, :building, :phone_num 
+  attr_accessor :user_id, :item_id, :post_cord, :prefecture_id, :city, :address, :building, :phone_num, :token
   
+
   with_options presence: true do
     validates :post_cord,     format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
     validates :city
     validates :address
     validates :phone_num, numericality: { lonly_integer: true}, length: { maximum: 11 }
+    validates :token
   end
 
   def save
    purchase = Purchase.create(user_id: user_id, item_id: item_id)
-   Address.ceate(post_cord: post_cord, prefecture_id: prefecture_id, city: city, address: address, building: building, phone_num: phone_num, purchase_id: purchase_id)
+   Address.create(post_cord: post_cord, prefecture_id: prefecture_id, city: city, address: address, building: building, phone_num: phone_num, purchase_id: purchase.id)
   end
 
 end
